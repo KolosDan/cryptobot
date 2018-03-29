f34c213d65b62d09b61b06b37bf8a3f5737f4122
artyemk <kolodaio@gmail.com>
2018-03-29 11:43:34 +0500

    updated backend

--- backend.py
+++ backend.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[65]:
@@ -19,16 +18,15 @@ db = MongoClient('213.183.48.143').cryptobot
 
 
 def create_user(_id, username):
-    user = {}
-    user['id'] = _id
-    user['username'] = username
-    user['eth_addr'] = None
-    user['email'] = None
-    user['phone'] = None
-    user['permissions'] = {'is_expert': False, 'is_admin': False}
-    user['balance'] = 0.0
-    user['operations'] = ['Created user ' + str(datetime.datetime.now())]
-    user['deposit_addr'] = requests.post('https://api.blockcypher.com/v1/eth/main/addrs').json()
-    
-    return db.user.insert_one(user)
-
+    if db.user.find_one({"id":int(_id)}) == None:
+        user = {}
+        user['id'] = _id
+        user['username'] = username
+        user['eth_addr'] = None
+        user['email'] = None
+        user['phone'] = None
+        user['permissions'] = {'is_expert': False, 'is_admin': False}
+        user['balance'] = 0.0
+        user['operations'] = ['Created user ' + str(datetime.datetime.now())]
+        user['deposit_addr'] = requests.post('https://api.blockcypher.com/v1/eth/main/addrs').json()
+        return db.user.insert_one(user)
