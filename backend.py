57b4aae5163ef114994a8ab299a36058fb0391ce
KolosDan <dan.danya99@gmail.com>
2018-03-29 13:32:01 +0500

    Added update func

--- backend.py
+++ backend.py
@@ -30,3 +30,19 @@ def create_user(_id, username):
         user['operations'] = ['Created user ' + str(datetime.datetime.now())]
         user['deposit_addr'] = requests.post('https://api.blockcypher.com/v1/eth/main/addrs').json()
         return db.user.insert_one(user)
+
+def update(_id,field, upd):
+    if field == 'eth_addr':
+        if validation.is_0x_prefixed(upd) and validation.is_address(upd) and len(upd) == 42:
+            return db.user.update({'id':_id},{'$set':{'eth_addr': upd}})
+    if field == 'email':
+        if re.match(r"[^@]+@[^@]+\.[^@]+", upd):
+            return db.user.update({'id':_id},{'$set':{'email': upd}})
+    if field == 'phone':
+        try:
+            if is_possible_number(parse('+11235125520')):
+                return db.user.update({'id':_id},{'$set':{'phone': upd}})
+            else:
+                return False
+        except:
+            return False
